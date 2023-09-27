eacafc6809bcd42cf4fcad55bbeb006c613a0919
ismet <ismetaba@outlook.com>
2023-09-27 23:27:25 +0300

    Initial commit

---
+++ backend/src/main/java/org/ai4hr/ai4hr/ServerStart.java
@@ -0,0 +1,13 @@
+package org.ai4hr.ai4hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerStart {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerStart.class, args);
+    }
+
+}
